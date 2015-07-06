9f99fccce7cdc162300af0b432eb6a4d63e6d3ac
pgbo <460667915@qq.com>
2015-07-07 01:22:45 +0800

    编写Crawler从后台获取原数据的代码

--- WukongSNS/Crawler/VC/ViewController.swift
+++ WukongSNS/Crawler/VC/ViewController.swift
@@ -26,15 +26,41 @@ class ViewController: UIViewController {
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
+        // 获取原数据
         SVProgressHUD.showWithStatus("获取原数据中...")
+        
+        let groupCountQuery = AVQuery(className: "RoleGroup")
+        groupCountQuery.countObjectsInBackgroundWithBlock { [weak self] (groupCount:Int, error:NSError!) -> Void in
+            if let strongSelf = self {
+                println("groupCount:\(groupCount), error:\(error.description)")
+                dispatch_async(dispatch_get_main_queue()){
+                    strongSelf.originRoleGroupCountLabel?.text = String("\(groupCount)")
+                }
+                let roleCountQuery = AVQuery(className: "Role")
+                roleCountQuery.countObjectsInBackgroundWithBlock({ [weak strongSelf] (roleCount:Int, error:NSError!) -> Void in
+                    if let inStrongSelf = strongSelf {
+                        println("roleCount:\(roleCount), error:\(error.description)")
+                        dispatch_async(dispatch_get_main_queue()){
+                            inStrongSelf.originRoleCountLabel?.text = String("\(roleCount)")
+                            if error != nil {
+                                SVProgressHUD.showErrorWithStatus(error.description)
+                            } else {
+                                SVProgressHUD.showSuccessWithStatus("已经存在\(groupCount)个角色组, \(roleCount)个角色")
+                            }
+                        }
+                    }
+                })
+            }
+        }
+        
     }
 
     private func startCrawl() {
-        
+        // TODO:
     }
     
     private func uploadData() {
-        
+        // TODO:
     }
 }
 
